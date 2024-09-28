38a2527c0b1c00ba63a8510c3e77a80449e58ea2
Mohammed Amaan <mohammedamaan79@gmail.com>
2024-09-28 18:24:58 +0530

    one more dummy commit for ci/cd

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      {/* Dummy Commit for CI/CD */}
-      <h1>Vite + React</h1>
+      <h1>Vite + React - Dummy Commit for CI/CD</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}

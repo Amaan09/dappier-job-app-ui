e929efd8efb4db9db3233a2ba9c81b24f7d5812a
Mohammed Amaan <mohammedamaan79@gmail.com>
2024-10-01 22:42:33 +0530

    create loader component and use it

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,27 +1,27 @@
-import { Suspense } from 'react';
-import { ErrorBoundary } from 'react-error-boundary';
-import { Outlet, useLocation } from 'react-router-dom';
-import { Dashboard } from '../pages/dashboard/dashboard';
-import { Spinner } from '../shared/spinner';
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { Outlet, useLocation } from "react-router-dom";
+import { Dashboard } from "../pages/dashboard/dashboard";
+import { Loader } from "../shared/loader";
 
 export const AppRoot = () => {
-  const location = useLocation();
-  return (
-    <Dashboard >
-      <Suspense
-        fallback={
-          <div className="flex size-full items-center justify-center">
-            <Spinner />
-          </div>
-        }
-      >
-        <ErrorBoundary
-          key={location.pathname}
-          fallback={<div>Something went wrong!</div>}
-        >
-          <Outlet />
-        </ErrorBoundary>
-      </Suspense>
-    </Dashboard>
-  );
+    const location = useLocation();
+    return (
+        <Dashboard>
+            <Suspense
+                fallback={
+                    <div className="flex size-full items-center justify-center">
+                        <Loader />
+                    </div>
+                }
+            >
+                <ErrorBoundary
+                    key={location.pathname}
+                    fallback={<div>Something went wrong!</div>}
+                >
+                    <Outlet />
+                </ErrorBoundary>
+            </Suspense>
+        </Dashboard>
+    );
 };
--- src/shared/spinner.tsx
+++
@@ -1,5 +0,0 @@
-export const Spinner = () => {
-    return (
-        <h1>This is a spinner component!</h1>
-    );
-}

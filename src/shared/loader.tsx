6a7fbabb64a15760edeaf1dad91dce7945e716a9
Mohammed Amaan <mohammedamaan79@gmail.com>
2024-10-04 15:48:33 +0530

    change loader backdrop opacity

--- src/shared/loader.tsx
+++ src/shared/loader.tsx
@@ -14,7 +14,7 @@ export const Loader = ({ size, fixed }: LoaderProps) => {
             role="status"
             className={
                 fixed
-                    ? "fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
+                    ? "fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-20"
                     : ""
             }
         >
